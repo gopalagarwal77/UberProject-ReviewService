e3f8c69102429d2b857e90ce109e23ec9e13a286
Gopal Agarwal <agarwalgopal247@gmail.com>
2025-05-12 23:08:29 +0530

    Added booking associated to review;

--- src/main/java/com/example/UberReviewService/services/ReviewService.java
+++ src/main/java/com/example/UberReviewService/services/ReviewService.java
@@ -1,11 +1,14 @@
 package com.example.UberReviewService.services;
 
+import com.example.UberReviewService.models.Booking;
 import com.example.UberReviewService.models.Review;
+import com.example.UberReviewService.repositories.BookingRepository;
 import com.example.UberReviewService.repositories.ReviewRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 
@@ -13,19 +16,37 @@ import java.util.Date;
 public class ReviewService implements CommandLineRunner {
 
     private ReviewRepository reviewRepository;
+    private BookingRepository bookingRepository;
 
-    public ReviewService(ReviewRepository reviewRepository){
+
+    public ReviewService(ReviewRepository reviewRepository , BookingRepository bookingRepository){
           this.reviewRepository=reviewRepository;
+          this.bookingRepository=bookingRepository;
     }
+
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("****************");
-       Review r = Review.builder()
-               .content("Amazing ride quality")
+//       Review r = Review.builder()
+//               .content("Amazing ride quality")
+//
+//               .rating(5.0)
+//               .build();  // code to create plain java object
+//
+//        Booking b = Booking.builder()
+//                .review(r)
+//                        .endTime(new Date())
+//                                .build();
+//
+//        bookingRepository.save(b);   // we should create a review first and then booking because booking  depended on review
+//        reviewRepository.save(r);
 
-               .rating(5.0)
-               .build();  // code to create plain java object
+      Optional<Booking> b = bookingRepository.findById(2L);
 
-        reviewRepository.save(r);
+      if(b.isPresent()){
+          System.out.println(b.get());
+          bookingRepository.delete(b.get());
+     }
     }
 }
